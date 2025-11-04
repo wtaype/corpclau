89d7a10e5fa965bcc29383dd5893299694b25031
wtaype <wilder.taype16@gmail.com>
2025-11-03 19:29:19 -0500

    Actualizacion Principal v0311:1929

--- smile/smiletop.js
+++ smile/smiletop.js
@@ -4,7 +4,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import { auth, db } from '../firebase/init.js';
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { getDocs, doc, updateDoc, deleteDoc, collection, query, where, orderBy, limit, startAfter, serverTimestamp, addDoc, setDoc } from "firebase/firestore";
-import { Capi, Mensaje, Notificacion, savels, getls, removels, wiTema, infoo, fechaLetra,mis10 } from '../widev.js';
+import { Capi, Mensaje, Notificacion, savels, getls, removels, wiTema, infoo, fechaLetra,mis10 } from './widev.js';
 
 // ========================================
 // 🔐 VARIABLES GLOBALES COMPACTAS
